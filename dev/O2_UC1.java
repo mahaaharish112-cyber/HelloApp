c1486371494457c2bba4823f56db9b3313aa4aa7
mahaaharish112-cyber <mahaaharish112@gmail.com>
2026-03-26 09:35:13 +0530

    ALL dev files are Added

---
+++ dev/O2_UC1.java
@@ -0,0 +1,6 @@
+package dev;
+public class O2_UC1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ dev/O8_UC7.java
@@ -0,0 +1,20 @@
+package dev;
+public class O8_UC7 {
+
+    public static void main(String[] args) {
+
+        String greeting;
+
+        // Check if no arguments are provided
+        if (args.length == 0) {
+            greeting = "Hello, World!";
+        } else {
+            // Join all arguments with ", "
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
+        }
+
+        // Print the greeting
+        System.out.println(greeting);
+    }
+}
